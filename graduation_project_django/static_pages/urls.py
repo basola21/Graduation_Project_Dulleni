122fc678ae092ea41f685cfd0e5812e5e59d1a7d
Basel <basel21mahmoud@gmail.com>
2023-03-26 23:27:07 +0200

    adding static pages not combatible yet

--- graduation_project_django/static_pages/urls.py
+++ graduation_project_django/static_pages/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home ,about, blog, contact, faqs, jobs
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
+    path('blog', blog, name='blog'),
+    path('contact', contact, name='contact'),
+    path('faqs', faqs, name='faqs'),
+    path('jobs', jobs, name='jobs'),
 ]
--- graduation_project_django/static_pages/views.py
+++ graduation_project_django/static_pages/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+
 
 def home(request):
     return render(request , "index.html")
+
+def about(request):
+    return render(request , "about.html")
+
+def blog(request):
+    return render(request , "blog.html")
+
+def contact(request):
+    return render(request , "contact.html")
+
+def faqs(request):
+    return render(request , "faqs.html")
+
+def jobs(request):
+    return render(request , "jobs.html")
